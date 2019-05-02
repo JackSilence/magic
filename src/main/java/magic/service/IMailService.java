62efdb4a3dcf6c402821ec600071ae057d10876d
JackSilence <jacksilence@gmail.com>
2019-05-02 16:02:17 +0800

    init

---
+++ src/main/java/magic/service/IMailService.java
@@ -0,0 +1,5 @@
+package magic.service;
+
+public interface IMailService {
+	void send( String subject, String content );
+}
---
+++ src/main/java/magic/service/IService.java
@@ -0,0 +1,5 @@
+package magic.service;
+
+public interface IService {
+	void exec();
+}
