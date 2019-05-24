1e182d51179b57894aebef741c397196fda857e3
JackSilence <jacksilence@gmail.com>
2019-05-24 11:13:36 +0800

    add AsyncExecutor

--- src/main/java/magic/controller/ExecuteController.java
+++ src/main/java/magic/controller/ExecuteController.java
@@ -2,49 +2,26 @@ package magic.controller;
 
 import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import magic.service.IService;
+import magic.service.AsyncExecutor;
 import magic.service.Slack;
 
 @RestController
-@EnableAsync
 public class ExecuteController {
-	private final Logger log = LoggerFactory.getLogger( this.getClass() );
-
 	@Autowired
-	private ApplicationContext context;
+	private AsyncExecutor executor;
 
 	@Autowired
 	private Slack slack;
 
-	@PostMapping( value = "/execute/{name}" )
-	@Async
+	@PostMapping( "/execute/{name}" )
 	public Map<String, String> execute( @PathVariable String name ) {
-		try {
-			Object bean = context.getBean( name );
+		executor.exec( name );
 
-			Assert.isInstanceOf( IService.class, bean );
-
-			log.error( "Execute task manually: " + name );
-
-			( ( IService ) bean ).exec();
-
-			return slack.text( "OK!" );
-
-		} catch ( Exception e ) {
-			log.error( "", e );
-
-			return slack.text( "Oops!" );
-		}
+		return slack.text( "OK!" );
 	}
 }
