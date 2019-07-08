ba71cd8524a9c4244bb6b9d55c57bde3b6b4ef2f
JackSilence <jacksilence@gmail.com>
2019-07-08 17:04:36 +0800

    add explicit method

--- src/main/java/magic/service/Cloudinary.java
+++ src/main/java/magic/service/Cloudinary.java
@@ -1,19 +1,41 @@
 package magic.service;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
+import com.cloudinary.Uploader;
 import com.cloudinary.utils.ObjectUtils;
+import com.cloudinary.utils.StringUtils;
 
 @Service
 public class Cloudinary {
 	public String upload( Object file, String publicId ) {
 		try {
-			return new com.cloudinary.Cloudinary().uploader().upload( file, ObjectUtils.asMap( "public_id", publicId ) ).get( "secure_url" ).toString();
+			return url( uploader().upload( file, ObjectUtils.asMap( "public_id", publicId ) ) );
 
 		} catch ( IOException e ) {
 			throw new RuntimeException( e );
+
 		}
+	}
+
+	public String explicit( String publicId ) {
+		try {
+			return url( uploader().explicit( publicId, ObjectUtils.asMap( "type", "upload" ) ) );
+
+		} catch ( IOException | RuntimeException e ) {
+			return StringUtils.EMPTY;
+
+		}
+	}
+
+	private Uploader uploader() {
+		return new com.cloudinary.Cloudinary().uploader();
+	}
+
+	private String url( Map<?, ?> map ) {
+		return map.get( "secure_url" ).toString();
 	}
 }
